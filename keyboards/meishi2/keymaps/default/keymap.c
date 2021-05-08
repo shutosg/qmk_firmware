19f8a53d5e809a3b132d968954d5b6a62f7de7bb
shutosg <shutoreds@gmail.com>
2021-05-09 03:56:47 +0900

    meishi2

--- keyboards/meishi2/keymaps/default/keymap.c
+++ keyboards/meishi2/keymaps/default/keymap.c
@@ -17,7 +17,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base */
-    LCTL(KC_Z),  LCTL(KC_X),  LCTL(KC_C), LCTL(KC_V) \
+    LGUI(KC_A),  LGUI(KC_X),  LGUI(KC_C), LGUI(KC_V) \
   )
 };
 
